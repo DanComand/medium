0e9fc420eb1078a9b4785b5a0029092701964d3c
Dan Comand <dcomand@gmail.com>
2015-04-25 23:49:30 -0400

    making posts show at /posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create!(
+	:title => "Dan's First Blog Post",
+	:author => "Dan Comand",
+	:content => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	:url => "https://pbs.twimg.com/profile_images/533116568293900288/ygg9UuN8_400x400.jpeg"
+
+
+
+	)
