c593fa8074ffdd0f19c5751ff4dcdda417c41337
Dan Comand <dcomand@gmail.com>
2015-04-26 00:50:29 -0400

    refactoring to save new posts in database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,24 @@ class PostsController < ApplicationController
 	end
 
 	def create
-    
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to posts_url
+		else
+			render :new
+    end
+  end
 
     # render :text => "Saving a picture. Title: #{params[:title]}, Author: #{params[:author]}, url: #{params[:url]}, content: #{params[:content]}"
+
+
+  private
+  def post_params
+  	params.require(:post).permit(:title, :author, :url, :content)
   end
+
+
+
 end
 
 
