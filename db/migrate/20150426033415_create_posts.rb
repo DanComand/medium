6c7a8ff9263b72aa65f0b05232d874f74bb92d8d
Dan Comand <dcomand@gmail.com>
2015-04-25 23:35:56 -0400

    creating Post model in database.

---
+++ db/migrate/20150426033415_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :author
+      t.text :content
+      t.string :url
+
+      t.timestamps null: false
+    end
+  end
+end
