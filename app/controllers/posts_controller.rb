cdb45578161dda67c668167d4ed5da8f2408bae2
Dan Comand <dcomand@gmail.com>
2015-04-26 01:36:09 -0400

    added feature to edit posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
@@ -18,6 +19,20 @@ class PostsController < ApplicationController
 		else
 			render :new
     end
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+
+  	if @post.update_attributes(post_params)
+  		redirect_to "/posts/#{@post.id}"
+  	else
+  		render :edit
+  	end
   end
 
     # render :text => "Saving a picture. Title: #{params[:title]}, Author: #{params[:author]}, url: #{params[:url]}, content: #{params[:content]}"
